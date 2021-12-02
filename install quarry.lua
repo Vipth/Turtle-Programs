37ab3b2e790e5f60c87389def789c5584b79e49c
Vipth <bradycurtis9@yahoo.com>
2021-12-01 22:01:08 -0600

    Push

---
+++ install quarry.lua
@@ -0,0 +1,4 @@
+print("Installing Quarry...")
+shell.run("wget https://raw.githubusercontent.com/Vipth/Turtle-Programs/main/Quarry.lua%20quarry")
+print("Installing Refuel...")
+shell.run("wget https://raw.githubusercontent.com/Vipth/Turtle-Programs/main/Refuel.lua")
