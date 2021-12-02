d259f1bdf9606d0b3d350fbab2bbe4f0a2621ae5
Vipth <bradycurtis9@yahoo.com>
2021-12-01 21:29:11 -0600

    Push

--- update quarry.lua
+++ update quarry.lua
@@ -1,25 +1,25 @@
 local FILENAME = '/quarry.lua'
- 
+
 local cacheBreak = tostring(math.random(0, 99999))
- 
+
 res, err = http.get('https://raw.githubusercontent.com/Vipth/Turtle-Programs/main/Quarry.lua?breaker=' .. cacheBreak)
 if not res then error(err) end
- 
+
 local code = res.readAll()
- 
- 
+
+
 if not(fs.exists(FILENAME))
 then
     local newHarvest = fs.open(FILENAME, 'w')
     newHarvest.close()
 end
- 
+
 local readFile = fs.open(FILENAME, 'r')
 local oldCode = readFile.readAll()
 readFile.close()
- 
+
 local file = fs.open(FILENAME, 'w')
- 
+
 if oldCode == code
 then
     file.write(oldCode)
@@ -28,7 +28,7 @@ else
     file.write(code)
     print('WRITING UPDATE')
     byteDiff = string.len(code) - string.len(oldCode)
- 
+
     if byteDiff >= 0
     then
         print(tostring(math.abs(byteDiff)) .. ' bytes added')
@@ -36,6 +36,6 @@ else
         print(tostring(math.abs(byteDiff)) .. ' bytes removed')
     end
 end
- 
+
 file.close()
 res.close()
