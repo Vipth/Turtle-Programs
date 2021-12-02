1f43a630cb3c07782921ea3055cd07b7b5132bbe
Vipth <bradycurtis9@yahoo.com>
2021-12-01 20:56:07 -0600

    Push

--- Quarry.lua
+++ Quarry.lua
@@ -1,2 +1,36 @@
--- Check Fuel
-shell.run('refuel')
+-- Global Variables
+NUM_SLOTS = 16
+
+-- Filter Items We Don't Want
+local trash_items = {
+    'minecraaft:cobblestone',
+    'minecraft:dirt',
+    'minecraft:diorite',
+    'minecraft:andesite',
+    'minecraft:granite',
+    'minecraft:gravel',
+    'minecraft:sand',
+    'minecraft:sandstone',
+    'chisel:limestone2',
+}
+
+-- Function To Throw Away Trash Items
+function disposeTrashItems()
+    local slotsWithTrash
+    for i = 1, NUM_SLOTS do
+        if turtle.getItemDetail(i) ~= nil then
+            for _, v in pairs(trash_items) do
+                if turtle.getItemDetail(i)["name"] == v then
+                    table.insert(slotsWithTrash, #slotsWithTrash + 1, i)
+                end
+            end
+        end
+    end
+
+    for i = 1, #slotsWithTrash do
+        turtle.select(slotsWithTrash[i])
+        turtle.drop()
+    end
+end
+
+disposeTrashItems()
