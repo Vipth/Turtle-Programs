6653dea1c962c4634f357f977b32d036d46ced84
Vipth <bradycurtis9@yahoo.com>
2021-12-01 21:02:01 -0600

    Push

--- Quarry.lua
+++ Quarry.lua
@@ -3,7 +3,7 @@ NUM_SLOTS = 16
 
 -- Filter Items We Don't Want
 local trash_items = {
-    'minecraaft:cobblestone',
+    'minecraft:cobblestone',
     'minecraft:dirt',
     'minecraft:diorite',
     'minecraft:andesite',
@@ -16,7 +16,7 @@ local trash_items = {
 
 -- Function To Throw Away Trash Items
 function disposeTrashItems()
-    local slotsWithTrash
+    local slotsWithTrash = {}
     for i = 1, NUM_SLOTS do
         if turtle.getItemDetail(i) ~= nil then
             for _, v in pairs(trash_items) do
---
+++ update quarry.lua
@@ -0,0 +1,41 @@
+local FILENAME = '/quarry.lua'
+ 
+local cacheBreak = tostring(math.random(0, 99999))
+ 
+local res, err = http.get('https://raw.githubusercontent.com/Vipth/Turtle-Programs/main/Quarry.lua?breaker=' .. cacheBreak)
+if not res then error(err) end
+ 
+local code = res.readAll()
+ 
+ 
+if not(fs.exists(FILENAME))
+then
+    local newHarvest = fs.open(FILENAME, 'w')
+    newHarvest.close()
+end
+ 
+local readFile = fs.open(FILENAME, 'r')
+local oldCode = readFile.readAll()
+readFile.close()
+ 
+local file = fs.open(FILENAME, 'w')
+ 
+if oldCode == code
+then
+    file.write(oldCode)
+    print('NO CHANGES MADE - Same Code')
+else
+    file.write(code)
+    print('WRITING UPDATE')
+    local byteDiff = string.len(code) - string.len(oldCode)
+ 
+    if byteDiff >= 0
+    then
+        print(tostring(math.abs(byteDiff)) .. ' bytes added')
+    else
+        print(tostring(math.abs(byteDiff)) .. ' bytes removed')
+    end
+end
+ 
+file.close()
+res.close()
